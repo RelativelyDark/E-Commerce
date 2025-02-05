ffdd45a231735ab2c30c8d4b0d0e2c7330be4c57
Rohit-cars24 <r.rohit@cars24.com>
2025-02-05 16:35:37 +0530

    Initial Commit

---
+++ BackEnd/FullStack/src/test/java/com/cars24/fullstack/FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cars24.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
